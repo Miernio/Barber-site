20f5cf66d939b6348d4043442a06c3851b6f3887
Wiktor Miernik <102859974+Miernio@users.noreply.github.com>
2023-02-03 19:22:00 +0100

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+const hamburger=document.querySelector(".hamburger")
+const navMenu=document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n=>
+    n.addEventListener("click",()=>{
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+
+const phone=document.querySelector('contact-phone');
+phone.addEventListener("click",()=>{
+    console.log("Funkcja w budowie")
+})
